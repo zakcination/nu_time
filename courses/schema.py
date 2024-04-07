2ca7e45bc8ecf2a48aa888b6afe187c439900913
Yerkebulan Serikov <yerkebulanserikov@MacBook-Air-Yerkebulan.local>
2024-04-07 20:18:32 +0500

    second graphql api

--- courses/schema.py
+++ courses/schema.py
@@ -2,7 +2,9 @@ import graphene
 import django_filters
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from .models import Course
+from .models import (
+    Course, Semester, Section, CourseGetting
+)
 
 class CourseFilter(django_filters.FilterSet):
     class Meta: 
@@ -22,6 +24,47 @@ class CourseType(DjangoObjectType):
         filterset_class = CourseFilter
         interfaces = (graphene.Node, )
 
+class SemesterFilter(django_filters.FilterSet):
+    class Meta:
+        model = Semester
+        fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'start_from': ['exact', 'lt', 'gt'],
+            'end_at': ['exact', 'lt', 'gt'],
+        }
+
+class SemesterType(DjangoObjectType):
+    class Meta:
+        model = Semester
+        filterset_class = SemesterFilter
+        interfaces = (graphene.Node, )
+
+class SectionFilter(django_filters.FilterSet):
+    class Meta:
+        model = Section
+        fields = {
+            'semester': ['exact'],
+            'course': ['exact'],
+            'section_number': ['exact', 'lt', 'gt'],
+            'instructors': ['exact'],
+            'section_type': ['exact', 'icontains', 'istartswith'],
+            'enrolled': ['exact', 'lt', 'gt'],
+            'capacity': ['exact', 'lt', 'gt'],
+            'time': ['exact', 'icontains', 'istartswith'],
+        }
+
+class SectionType(DjangoObjectType):
+    class Meta:
+        model = Section
+        filterset_class = SectionFilter
+        interfaces = (graphene.Node, )
+
 class Query(graphene.ObjectType):
     course = graphene.Node.Field(CourseType)
     all_courses = DjangoFilterConnectionField(CourseType)
+    
+    semester = graphene.Node.Field(SemesterType)
+    all_semesters = DjangoFilterConnectionField(SemesterType)
+
+    section = graphene.Node.Field(SectionType)
+    all_sections = DjangoFilterConnectionField(SectionType)
